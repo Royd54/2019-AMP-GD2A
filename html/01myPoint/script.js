dd05559cd61bd785b09078d3cd0564cdf469d2ee
Royd54 <42915597+Royd54@users.noreply.github.com>
2019-09-09 12:27:47 +0200

    af
    af

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,32 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let woekoe = [];
+
+Animate();
+
+function Animate(){
+    context.clearRect(0,0,width,height);
+    requestAnimationFrame(Animate);
+    let color = "rgb(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + ")";
+    let borderColor = "rgb(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + ")";
+    woekoe.push(new Point(new Vector2d(getRandom(width),getRandom(height)), getRandom(200), color, borderColor));
+    
+    for(let i = 0; i<woekoe.length; i++){
+        woekoe[i].label = i;
+        woekoe[i].radius++;
+        woekoe[i].draw(context);
+        if(woekoe[i].radius > 100){
+          woekoe.splice(i,1);
+        }
+      }
+    console.log("loko");
+    console.log(woekoe.length);
+
+}
+
+function getRandom(max){
+    let ans = Math.floor(Math.random()*max);
+    return ans;
+}
